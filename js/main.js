7b5dac468e27400f053557c2ee1a99cda23a2c5a
aesposito12 <angelaesposito@Angelas-MacBook-Pro.local>
2014-10-09 13:27:35 -0400

    Demo 3

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // initialize animated anchor scroll
 // Remove /* and */ to initiate the plugin
-/* $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-}); */
+//* $(function() {
+//    $('a[href*=#]:not([href=#])').click(function() {
+//        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//            var target = $(this.hash);
+//            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//            if (target.length) {
+//                $('html,body').animate({
+//                    scrollTop: target.offset().top
+//                }, 1000);
+//                return false;
+//            }
+//        }
+//    });
+//}); */
 
 
 // initialize fitvids.js
@@ -25,7 +25,7 @@
 
 // initialize bigtext.js
 // Remove /* and */ to initiate the plugin
-/* $(function() {
+ $(function() {
     WebFont.load({
         custom: {
             families: ['cubano'], // font-family name
@@ -38,17 +38,17 @@
             $('#webapps-headline').bigtext();
         }
     });
-}); */
+}); 
 
 
 // initialize Menu Button Expand/Collapse
 // Remove /* and */ to initiate the plugin
-/* $(document).ready(function(){
+$(document).ready(function(){
     $(".toggle-button").next("ul").hide();
     $(".toggle-button").click(function(){
         $(".toggle-button").next("ul").slideToggle();
     });
-});*/
+});
 
 
 // initialize responsiveslides.js
